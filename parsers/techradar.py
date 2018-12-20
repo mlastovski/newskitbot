c8fd0b544c619909786a9f655ab55d000a1f29ad
mlastovski <mark.lastovskyy@gmail.com>
2018-12-20 23:21:30 +0200

    techradar v2

--- parsers/techradar.py
+++ parsers/techradar.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 import re
-from parse_tool import extract_keywords
+from parsers.parse_tool import extract_keywords
 
 debug = False
 test = True
